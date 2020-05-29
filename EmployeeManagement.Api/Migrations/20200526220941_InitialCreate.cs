b01c3bc4fb2ff176628f4ba0b71030679d5bd980
atip chao <atip/chaogunhar@cookcountyil.gov>
2020-05-28 20:26:29 -0500

    Initial Commit

---
+++ EmployeeManagement.Api/Migrations/20200526220941_InitialCreate.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    Gender = table.Column<int>(nullable: false),
+                    DepartmentId = table.Column<int>(nullable: false),
+                    PhotoPath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "DepartmentId", "DepartmentName" },
+                values: new object[,]
+                {
+                    { 1, "IT" },
+                    { 2, "HR" },
+                    { 3, "Payroll" },
+                    { 4, "Admin" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "DateOfBirth", "DepartmentId", "Email", "FirstName", "Gender", "LastName", "PhotoPath" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1999, 9, 9, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "tt@gmail.com", "T", 1, "T", "images/trumpapporvd.jpg" },
+                    { 2, new DateTime(1956, 8, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "jj@gmail.com", "J", 0, "J", "images/trumpapporvd.jpg" },
+                    { 3, new DateTime(1987, 6, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, "kk@gmail.com", "K", 2, "K", "images/trumpapporvd.jpg" },
+                    { 4, new DateTime(1979, 3, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "ll@gmail.com", "L", 1, "L", "images/trumpapporvd.jpg" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ EmployeeManagement.Api/Models/AppDbContext.cs
@@ -0,0 +1,82 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Department>().HasData(new Department
+                { DepartmentId = 1, DepartmentName = "IT" });
+            modelBuilder.Entity<Department>().HasData(new Department
+                { DepartmentId = 2, DepartmentName = "HR" });
+            modelBuilder.Entity<Department>().HasData(new Department
+                { DepartmentId = 3, DepartmentName = "Payroll" });
+            modelBuilder.Entity<Department>().HasData(new Department
+                { DepartmentId = 4, DepartmentName = "Admin" });
+                
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "T",
+                LastName = "T",
+                Email = "tt@gmail.com",
+                DateOfBirth = new DateTime(1999, 9, 9),
+                Gender = Gender.Female,
+                DepartmentId = 2,
+                PhotoPath = "images/trumpapporvd.jpg"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "J",
+                LastName = "J",
+                Email = "jj@gmail.com",
+                DateOfBirth = new DateTime(1956, 8, 7),
+                Gender = Gender.Male,
+                DepartmentId = 1,
+                PhotoPath = "images/trumpapporvd.jpg"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "K",
+                LastName = "K",
+                Email = "kk@gmail.com",
+                DateOfBirth = new DateTime(1987, 6, 5),
+                Gender = Gender.Other,
+                DepartmentId = 3,
+                PhotoPath = "images/trumpapporvd.jpg"
+            });
+
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 4,
+                FirstName = "L",
+                LastName = "L",
+                Email = "ll@gmail.com",
+                DateOfBirth = new DateTime(1979, 3, 2),
+                Gender = Gender.Female,
+                DepartmentId = 1,
+                PhotoPath = "images/trumpapporvd.jpg"
+            });
+        }
+    }
+}
