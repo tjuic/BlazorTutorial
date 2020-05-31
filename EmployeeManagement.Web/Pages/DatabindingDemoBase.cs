594e0979fd5044897bf4242dc0bb83a47dcb97dd
atip chao <atip/chaogunhar@cookcountyil.gov>
2020-05-31 18:30:50 -0500

    vid 26 two way binding

--- EmployeeManagement.Web/Pages/DatabindingDemoBase.cs
+++ EmployeeManagement.Web/Pages/DatabindingDemoBase.cs
@@ -8,9 +8,6 @@ namespace EmployeeManagement.Web.Pages
 {
     public class DatabindingDemoBase : ComponentBase
     {
-        protected string Name { get; set; } = "TT";
-        protected string Gender { get; set; } = "Male";
-
-        protected string Colour { get; set; } = "background-color:white";
+        public string Description { get; set; } = string.Empty;
     }
 }
