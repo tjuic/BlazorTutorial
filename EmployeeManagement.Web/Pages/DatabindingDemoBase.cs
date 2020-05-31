58682580ce5fff2d6621ca7b240360ebdf2f5467
atip chao <atip/chaogunhar@cookcountyil.gov>
2020-05-31 18:22:51 -0500

    vid 25 binding

---
+++ EmployeeManagement.Web/Pages/DatabindingDemoBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DatabindingDemoBase : ComponentBase
+    {
+        protected string Name { get; set; } = "TT";
+        protected string Gender { get; set; } = "Male";
+
+        protected string Colour { get; set; } = "background-color:white";
+    }
+}
